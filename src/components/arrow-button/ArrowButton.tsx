33a55b78d58c11a84f06093db3c0aefdbd34b5a0
Vladislav Maltsev <malcelvad777@yandex.ru>
2024-09-02 13:21:00 +0300

    first review

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -2,18 +2,30 @@ import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
+import clsx from 'clsx';
+
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type TProps = {
+	isOpen: boolean;
+	onClick: (status: boolean) => void;
+};
+
+export const ArrowButton = ({ onClick, isOpen }: TProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, isOpen && styles.container_open)}
+			onClick={() => onClick(!isOpen)}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, isOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
